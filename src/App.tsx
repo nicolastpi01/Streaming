82788b6fa73fb54f89ba78339ce9f62f0307e8fc
OttoOctavius <OttoOctavius@users.noreply.github.com>
2020-04-14 17:22:19 -0300

    Agregado componente de video junto a un nuevo modulo ts para poder reproducir video

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
+import { Player } from 'video-react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h5>fuente original:https://media.w3.org/2010/05/sintel/trailer_hd.mp4</h5>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Player
+          playsInline
+          poster={logo}
+          src="https://localhost:5001/api/Video/getFileById?fileId=1"
+        />
       </header>
     </div>
   );

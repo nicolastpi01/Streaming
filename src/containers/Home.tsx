1860464ddd678d8e0a7400ce1089023b655cad9f
OttoOctavius <OttoOctavius@users.noreply.github.com>
2020-04-17 21:43:57 -0300

    Agregado ep de lista videos.

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Player , ControlBar, BigPlayButton, LoadingSpinner,CurrentTimeDisplay ,TimeDivider} from 'video-react';
 //import logo from '../logo.svg';
 //import { Stream } from 'stream';
 //import {Button} from 'react-bootstrap';
+import API from '../utils/api';
+import axios from 'axios';
+import { VideosResult } from '../types/VideosResult';
 
 const Home : React.FC = () => {
     const [idvideo, setidvideo] = React.useState<number>(0)
     const [reproductor,setreproductor] = React.useState<any|undefined>()
+    const [catalogo, setCatalogo] = React.useState<VideosResult[]>([]);
     //const [fuente, setfuente] = React.useState<ReadableStream>()
+
+    useEffect(() => {
+      console.log("fuera")
+      axios.get("https:\\localhost:5001/api/Video/videos",	{ headers: {
+        'Access-Control-Allow-Origin': '*' //,
+        //'Vary': 'Origin'
+        }
+      }).then( response => response.data ).then( (response: VideosResult[])=>{
+          console.log("dentro")
+          console.log(response)
+          
+          /*if (response.status !== 200) {
+            console.table(response);
+            return;
+          }*/
+          setCatalogo( response );
+      }
+    )
+    .catch(function(err) {
+      console.log(err);
+    });
+      
+    }, []);
 
     function sourceurl(id:string){
         return "https://localhost:5001/api/Video/getFileById?fileId="+ id
@@ -20,17 +47,16 @@ const Home : React.FC = () => {
 
     return <div>
       <header className="App-header">
-
+        <h1></h1>
         <select value={idvideo} defaultValue={0} onChange={e => onCambioVideo( parseInt((e.currentTarget.value)) ) }>
-            <option value={0}>Video1</option>
-            <option value={1}>Video2</option>
-            <option value={2}>Video3</option>
+            {catalogo.length > 0? catalogo.map(el=><option value={el.indice}>{el.nombre}</option>): <option value={0}>Sin datos</option>}
         </select>
 
         <div>
         <Player
           ref={ (player:any) => setreproductor(player) }
           fluid={true}
+          inline
           >
           <source src={sourceurl(idvideo.toString())} />
           <ControlBar autoHide={true}>

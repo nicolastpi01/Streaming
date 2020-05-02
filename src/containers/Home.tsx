c2c2b1820fe7ee78809a431b8727163679acddc7
nicolastpi01 <nicolastpi10@gmail.com>
2020-05-01 22:45:26 -0300

    funciones para traer sugerencias de busq & videos por una descrip en barra de busqueda

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,33 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
-//import logo from '../logo.svg';
-//import { Stream } from 'stream';
-//import {Button} from 'react-bootstrap';
-import API from '../utils/api';
 import axios from 'axios';
 import { VideosResult } from '../types/VideosResult';
+import { searchSugestions, searchVideos } from '../APIs/mediaAPI';
 
 const Home : React.FC = () => {
     const [idvideo, setidvideo] = React.useState<number|undefined>()
     const [reproductor,setreproductor] = React.useState<any|undefined>()
     const [catalogo, setCatalogo] = React.useState<VideosResult[]>([]);
-    //const [fuente, setfuente] = React.useState<ReadableStream>()
+    const [search, setSearch] = useState<string>("")
+    const [searchSugestion, setSearchSugestion] = useState<string[]>([])
 
     useEffect(() => {//TODO:refactor y delegacion de responsabilidad
       console.log("fuera")
-      axios.get("https:\\localhost:5001/api/Video/videos",	{ headers: {
+      axios.get("https:\\localhost:5001/api/video/videos",	{ headers: {
         'Access-Control-Allow-Origin': '*' //,
         //'Vary': 'Origin'
-        }
-      }).then( response => response.data ).then( (response: VideosResult[])=>{
+      }
+      }).then(response => response.data ).then( (response: VideosResult[])=>{
           console.log("dentro")
           console.log(response)
-          
-          /*if (response.status !== 200) {
-            console.table(response);
-            return;
-          }*/
-          setCatalogo( response );
+          setCatalogo(response);
       }
     )
     .catch(function(err) { //TODO:Reemplazar por componente visual
@@ -42,21 +35,72 @@ const Home : React.FC = () => {
     
     function onCambioVideo(newNumber:number) {
         setidvideo(newNumber);
-        //reproductor.load();
+        
     }
+
+    const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+      console.log(event.target.value);
+      // Llama al metodo en el back que carga sugerencias
+      setSearch(event.target.value);
+      getSugestions();
+    }
+
+    const getSugestions = async () => {
+      searchSugestions(search).then(sugestions =>{
+        setSearchSugestion(sugestions)
+        console.log(sugestions)
+      }).catch(e => console.log("ERROR BUSCANDO LAS SUGERENCIAS" + e))
+    }
+  
+    // Llama a la API que busca los videos
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      searchVideos(search).then(videos =>{
+        setCatalogo(videos)
+        onCambioVideo(parseInt(videos[0].indice)) // Despues se tiene que sacar !!!!
+        console.log(videos)
+      }).catch(e => console.log("ERROR BUSCANDO LOS VIDEOS" + e))
+      event.preventDefault();
+    }
+
 
     return <div>
       <header className="App-header">
-        <h1></h1>
-        <select value={idvideo} defaultValue={0} onChange={e => onCambioVideo( parseInt((e.currentTarget.value)) ) }>
+        
+        
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={search} onChange={handleChange}/>
+          <input type="submit" value="&#128269;"/>
+        </form>
+        
+        <br>
+        </br>
+        <br>
+        </br>
+        <br>
+        </br>
+        <br>
+        </br>
+        <br>
+        </br>
+
+        <select value={search} defaultValue="" onSelect={e => console.log(e.currentTarget.value)}>
+            {searchSugestions.length > 0 ? searchSugestion.map(s => <option value={s}>{s}</option>) : <option value={""}>Sin datos</option>}
+        </select>
+
+        <br>
+        </br>
+        <br>
+        </br>
+
+        <select value={idvideo} defaultValue={0} onClick={e => onCambioVideo(parseInt(e.currentTarget.value)) }>
             {catalogo.length > 0? catalogo.map(el=><option value={el.indice}>{el.nombre}</option>): <option value={0}>Sin datos</option>}
         </select>
 
         <div>
         <ReactPlayer
-          ref={ (player:any) => setreproductor(player) }
+          ref={(player:any) => setreproductor(player) }
           fluid={true}
-          url={idvideo !==undefined?sourceurl(idvideo.toString()):""}
+          url={idvideo !==undefined?sourceurl(idvideo.toString()) :""}
           controls={true}
           >
         </ReactPlayer>
@@ -66,10 +110,5 @@ const Home : React.FC = () => {
       </div>
 }
 
-/*          <ControlBar autoHide={true}>
-            <CurrentTimeDisplay order={4.1} />
-            <TimeDivider order={4.2} />
-            <BigPlayButton position="center" order={4.3}/>
-            <LoadingSpinner order={4.4}/>
-          </ControlBar> */
+
 export default Home

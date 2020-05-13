0b5e04ac66dfd44af8720d7501bc7ba46d40b72e
OttoOctavius <OttoOctavius@users.noreply.github.com>
2020-05-13 02:04:33 -0300

    Instaladas y corriendo base de test jest, falla por css

---
+++ src/__tests__/Home.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+import Home from "../containers/Home";
+
+describe("<Home />", () => {
+  test("should display a blank login form, with remember me checked by default", async () => {
+    
+  });
+});

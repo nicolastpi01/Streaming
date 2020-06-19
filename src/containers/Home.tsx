6fc7a54abbf5d8028acc3f45b25c9dcb459d4415
nicolastpi01 <nicolastpi10@gmail.com>
2020-06-19 20:27:56 -0300

    comenzando nuevo buscador de sugerencias

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -9,69 +9,24 @@ import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Pagination from '@material-ui/lab/Pagination';
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {
-      '& > * + *': {
-        marginTop: theme.spacing(2),
-        //justify: "justify-items-center",
-        //row justify-content-center justify-items-center
-      },
-    },
-  }),
-);
+
 
 
 const Home : React.FC = () => {
     
     
-    const [catalogo, setCatalogo] = useState<VideosResult[]>([]);
-    const [search, setSearch] = useState<string>("")
-    const [searchSugestion, setSearchSugestion] = useState<string[]>([])
-    const [show, setShow] = useState(false);
-    //const { getTokenSilently } = useAuth0();
-
-    // FOR PAGINING
-    const [offset, setOffset] = useState<number>(0) // cantidad de videos por pagina
-    const [size, setSize] = useState<number>(0) // cantidad de videos totales
-    //const [pages, setPages] = useState<number[]>([]) // todas las paginas, por ejemplo: [0,1,2,3,4,5,6]
-    const [pages, setPages] = useState<number>()
-    const [currentPage, setCurrentPage] = useState<number>(1) // La pagina donde esta, si cambia onScroll currentPage ++
-    //const [pagesToDisplay,] = useState(4)
-
-
-
-
-  // NUEVO INTENTO PAGINADO
-  const classes = useStyles();
-  //const [page, setPage] = React.useState(1);
-  const handleChangePage = (event: React.ChangeEvent<unknown>, value: number) => {
-    setCurrentPage(value);
-  };
     
 
-  // Llama a la API que busca todos los videos
- const getAllVideos = async (page: number) => {
-      
-  searchAllVideos(page).then(result => {
-    result.page.forEach( (videores :VideosResult) => 
-          videores.imagenURL = getImagen(videores.indice.toString())
-        );
-    setCatalogo(result.page)
-    setOffset(result.offset)
-    setSize(result.size)
-    setPages(Math.ceil(result.size / result.offset))
-  }).catch((e) => {})     
-} 
 
 
-useEffect((() => 
-  {
-    document.title = "Home"
-    console.log("THE CURRENT PAGE: " + currentPage)
-    getAllVideos(currentPage)
+
+  
     
-  }), [currentPage]);
+
+  
+
+
+
 
   /*
     const calculatePages = (cantPaginas: number) => {
@@ -86,12 +41,13 @@ useEffect((() =>
     // Llama a la API que busca las sugerencias
     const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
       //if(event.target.value === "") handleBlur() cuando el campo queda vacio deberia borrar las sugerencias
-      setSearch(event.target.value);
-      getSugestions();
+      //setSearch(event.target.value);
+      //getSugestions();
       
     }
 
     // Llama a la API para obtener las sugerencias
+    /*
     const getSugestions = async () => {
       searchSugestions(search).then(sugestions =>{
         setSearchSugestion(sugestions)
@@ -118,18 +74,19 @@ useEffect((() =>
         console.log("ERROR BUSCANDO LOS VIDEOS DESDE LA LISTA DE SUGERENCIAS" + e)
       }) 
     }
-
+    
     
     const handleBlur = () => {
       setSearchSugestion([])
     }
-    
+    */
+
     return (
       <div className="container-fluid">
 
-
+      {/* 
         <div className="container-fluid" style={{textAlign:"center", marginTop:"3px"}}>
-          <form onSubmit={handleSubmit} data-testid="busqueda-recomendaciones-submit" onBlur={handleBlur}> {/* ESTA ANDANDO MAL EL ONBLUR*/}
+          <form onSubmit={handleSubmit} data-testid="busqueda-recomendaciones-submit" onBlur={handleBlur}> 
             <input type="text" placeholder="Buscar.." value={search} onChange={handleChange} data-testid="busqueda-recomendaciones-texto" style={{width:"410px"}} />
             <input type="submit" value="&#128269;" data-testid="busqueda-recomendaciones-boton" />
           </form>
@@ -170,8 +127,9 @@ useEffect((() =>
       </div> 
 
 
-
+          */}
     </div>  
+    
     )
 }
 
--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import { Navbar, Form, Nav, Button } from "react-bootstrap";
 import history from "../utils/history";
+import Autosuggest, { SuggestionsFetchRequested } from 'react-autosuggest';
+import { searchSugestions } from "../APIs/mediaAPI";
 
 const NavBar = () => {
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
+    const [suggestions, setSuggestions] = useState<string[]>([])
+    const [search, setSearch] = useState<string>("")
+
+
+    // Teach Autosuggest how to calculate suggestions for any given input value.
+    /*
+    const getSuggestions = (value : string) => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+  
+    return inputLength === 0 ? [] : languages.filter(lang =>
+      lang.name.toLowerCase().slice(0, inputLength) === inputValue
+    );
+  }; */
+
+
+    
+
+    // Autosuggest will call this function every time you need to update suggestions.
+    const onSuggestionsFetchRequested = (request : any) => {
+        setSearch(request)
+        getSuggestions();
+        //setSuggestions([])
+    }
+
+    // Llama a la API para obtener las sugerencias
+    const getSuggestions = async () => {
+        searchSugestions(search).then(suggestions =>{
+          setSuggestions(suggestions)
+            }).catch((e:any) => {
+            console.log("ERROR BUSCANDO LAS SUGERENCIAS" + e);
+            });
+    }
+
+    
+    // Autosuggest will call this function every time you need to clear suggestions.
+    const onSuggestionsClearRequested = () => {
+        setSuggestions([])
+    }
+
+    const onChanged = (event: any,  newValue: any ) => {
+        setSearch(newValue)
+    }
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+        placeholder: 'Type a programming language',
+        value: '',
+        onChange: onChanged
+    }
+
+    // Use your imagination to render suggestions.
+    const renderSuggestion = (suggestion : string) => (
+        <div>
+            {suggestion}
+        </div>
+    )
+
+    const suggestion =  ['hola','chau','nos vimos','perri']
+    
+
+    // When suggestion is clicked, Autosuggest needs to populate the input
+    // based on the clicked suggestion. Teach Autosuggest how to calculate the
+    // input value for every given suggestion.
+    const getSuggestionValue = (suggestion: string) => suggestion;
 
 return (
 
@@ -23,9 +90,27 @@ return (
     <Nav.Link href="/"><p style={{color : "white", marginTop : "16px"}}>Streaming</p></Nav.Link>
     </Form>
     </Nav>
+
+
+    
+    <Nav className="mx-auto" >
+    <Form inline>
+    <Autosuggest
+        suggestions={suggestion}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+    />
+    <input type="submit" value="&#128269;" data-testid="busqueda-recomendaciones-boton"/>
+    </Form>
+    </Nav>
   
 
     <Form inline>
+
+    
   
     {!isAuthenticated && (
         <Button variant="outline-light" onClick={() =>

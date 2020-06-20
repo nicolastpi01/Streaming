663e401017aef931430d3053b6a17c61c8a09ad7
nicolastpi01 <nicolastpi10@gmail.com>
2020-06-20 00:40:11 -0300

    suggestion funcionando pero sin las sugerencias del back

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -2,76 +2,175 @@ import React, { useState } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import { Navbar, Form, Nav, Button } from "react-bootstrap";
 import history from "../utils/history";
-import Autosuggest, { SuggestionsFetchRequested } from 'react-autosuggest';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import { searchSugestions } from "../APIs/mediaAPI";
+
+interface Film {
+    inputValue?: string;
+    title : string,
+    year?: number,
+  }  
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films : Film[] = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
+    { title: 'The Good, the Bad and the Ugly', year: 1966 },
+    { title: 'Fight Club', year: 1999 },
+    { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
+    { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
+    { title: 'Forrest Gump', year: 1994 },
+    { title: 'Inception', year: 2010 },
+    { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
+    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+    { title: 'Goodfellas', year: 1990 },
+    { title: 'The Matrix', year: 1999 },
+    { title: 'Seven Samurai', year: 1954 },
+    { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
+    { title: 'City of God', year: 2002 },
+    { title: 'Se7en', year: 1995 },
+    { title: 'The Silence of the Lambs', year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: 'Life Is Beautiful', year: 1997 },
+    { title: 'The Usual Suspects', year: 1995 },
+    { title: 'Léon: The Professional', year: 1994 },
+    { title: 'Spirited Away', year: 2001 },
+    { title: 'Saving Private Ryan', year: 1998 },
+    { title: 'Once Upon a Time in the West', year: 1968 },
+    { title: 'American History X', year: 1998 },
+    { title: 'Interstellar', year: 2014 },
+    { title: 'Casablanca', year: 1942 },
+    { title: 'City Lights', year: 1931 },
+    { title: 'Psycho', year: 1960 },
+    { title: 'The Green Mile', year: 1999 },
+    { title: 'The Intouchables', year: 2011 },
+    { title: 'Modern Times', year: 1936 },
+    { title: 'Raiders of the Lost Ark', year: 1981 },
+    { title: 'Rear Window', year: 1954 },
+    { title: 'The Pianist', year: 2002 },
+    { title: 'The Departed', year: 2006 },
+    { title: 'Terminator 2: Judgment Day', year: 1991 },
+    { title: 'Back to the Future', year: 1985 },
+    { title: 'Whiplash', year: 2014 },
+    { title: 'Gladiator', year: 2000 },
+    { title: 'Memento', year: 2000 },
+    { title: 'The Prestige', year: 2006 },
+    { title: 'The Lion King', year: 1994 },
+    { title: 'Apocalypse Now', year: 1979 },
+    { title: 'Alien', year: 1979 },
+    { title: 'Sunset Boulevard', year: 1950 },
+    { title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', year: 1964 },
+    { title: 'The Great Dictator', year: 1940 },
+    { title: 'Cinema Paradiso', year: 1988 },
+    { title: 'The Lives of Others', year: 2006 },
+    { title: 'Grave of the Fireflies', year: 1988 },
+    { title: 'Paths of Glory', year: 1957 },
+    { title: 'Django Unchained', year: 2012 },
+    { title: 'The Shining', year: 1980 },
+    { title: 'WALL·E', year: 2008 },
+    { title: 'American Beauty', year: 1999 },
+    { title: 'The Dark Knight Rises', year: 2012 },
+    { title: 'Princess Mononoke', year: 1997 },
+    { title: 'Aliens', year: 1986 },
+    { title: 'Oldboy', year: 2003 },
+    { title: 'Once Upon a Time in America', year: 1984 },
+    { title: 'Witness for the Prosecution', year: 1957 },
+    { title: 'Das Boot', year: 1981 },
+    { title: 'Citizen Kane', year: 1941 },
+    { title: 'North by Northwest', year: 1959 },
+    { title: 'Vertigo', year: 1958 },
+    { title: 'Star Wars: Episode VI - Return of the Jedi', year: 1983 },
+    { title: 'Reservoir Dogs', year: 1992 },
+    { title: 'Braveheart', year: 1995 },
+    { title: 'M', year: 1931 },
+    { title: 'Requiem for a Dream', year: 2000 },
+    { title: 'Amélie', year: 2001 },
+    { title: 'A Clockwork Orange', year: 1971 },
+    { title: 'Like Stars on Earth', year: 2007 },
+    { title: 'Taxi Driver', year: 1976 },
+    { title: 'Lawrence of Arabia', year: 1962 },
+    { title: 'Double Indemnity', year: 1944 },
+    { title: 'Eternal Sunshine of the Spotless Mind', year: 2004 },
+    { title: 'Amadeus', year: 1984 },
+    { title: 'To Kill a Mockingbird', year: 1962 },
+    { title: 'Toy Story 3', year: 2010 },
+    { title: 'Logan', year: 2017 },
+    { title: 'Full Metal Jacket', year: 1987 },
+    { title: 'Dangal', year: 2016 },
+    { title: 'The Sting', year: 1973 },
+    { title: '2001: A Space Odyssey', year: 1968 },
+    { title: "Singin' in the Rain", year: 1952 },
+    { title: 'Toy Story', year: 1995 },
+    { title: 'Bicycle Thieves', year: 1948 },
+    { title: 'The Kid', year: 1921 },
+    { title: 'Inglourious Basterds', year: 2009 },
+    { title: 'Snatch', year: 2000 },
+    { title: '3 Idiots', year: 2009 },
+    { title: 'Monty Python and the Holy Grail', year: 1975 },
+  ];
+
+  /*
+                <><Autocomplete
+
+    value={value}
+
+    onChange={(event: any, newValue: Film | null) => {
+      setValue(newValue);
+      // Llama al sever para buscar el video
+    } }
+    inputValue={inputValue}
+
+    onInputChange={(event, newInputValue) => {
+
+      const inputValue = newInputValue.trim().toLowerCase();
+
+      const result = top100Films.filter(movie => movie.title.toLowerCase().includes(inputValue)
+      );
+
+      // el result sale de llamar al server
+      setInputValue(newInputValue);
+      setSuggestions(result);
+
+    } }
+
+    id="combo-box-demo"
+
+    options={suggestions}
+
+    getOptionLabel={(option: Film) => option.title}
+    style={{ width: 300 }}
+    renderInput={(params: any) => <TextField {...params} label="Combo box" variant="outlined" />} />
+
+    */
+  
+const filter = createFilterOptions<Film>();
 
 const NavBar = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
-    const [suggestions, setSuggestions] = useState<string[]>([])
-    const [search, setSearch] = useState<string>("")
-
-
-    // Teach Autosuggest how to calculate suggestions for any given input value.
-    /*
-    const getSuggestions = (value : string) => {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-  
-    return inputLength === 0 ? [] : languages.filter(lang =>
-      lang.name.toLowerCase().slice(0, inputLength) === inputValue
-    );
-  }; */
-
-
     
 
-    // Autosuggest will call this function every time you need to update suggestions.
-    const onSuggestionsFetchRequested = (request : any) => {
-        setSearch(request)
-        getSuggestions();
-        //setSuggestions([])
-    }
+    const [suggestions, setSuggestions] = useState<Film[]>([]);
+    //const [value, setvalue] = useState<string>('');
+    const [value, setValue] = useState<Film | null>(); // el objeto 
+    const [inputValue, setInputValue] = useState(''); // el string de busqueda
 
     // Llama a la API para obtener las sugerencias
-    const getSuggestions = async () => {
-        searchSugestions(search).then(suggestions =>{
-          setSuggestions(suggestions)
-            }).catch((e:any) => {
-            console.log("ERROR BUSCANDO LAS SUGERENCIAS" + e);
-            });
-    }
+    /*
+    const getSugestions = async () => {
+      searchSugestions(search).then(sugestions =>{
+        setSuggestions(sugestions)
+      }).catch((e) => {
+        console.log("ERROR BUSCANDO LAS SUGERENCIAS" + e);
+      });
+    }¨*/
 
-    
-    // Autosuggest will call this function every time you need to clear suggestions.
-    const onSuggestionsClearRequested = () => {
-        setSuggestions([])
-    }
-
-    const onChanged = (event: any,  newValue: any ) => {
-        setSearch(newValue)
-    }
-
-    // Autosuggest will pass through all these props to the input.
-    const inputProps = {
-        placeholder: 'Type a programming language',
-        value: '',
-        onChange: onChanged
-    }
-
-    // Use your imagination to render suggestions.
-    const renderSuggestion = (suggestion : string) => (
-        <div>
-            {suggestion}
-        </div>
-    )
-
-    const suggestion =  ['hola','chau','nos vimos','perri']
-    
-
-    // When suggestion is clicked, Autosuggest needs to populate the input
-    // based on the clicked suggestion. Teach Autosuggest how to calculate the
-    // input value for every given suggestion.
-    const getSuggestionValue = (suggestion: string) => suggestion;
 
 return (
 
@@ -93,17 +192,61 @@ return (
 
 
     
-    <Nav className="mx-auto" >
+    <Nav className="mx-auto">
     <Form inline>
-    <Autosuggest
-        suggestions={suggestion}
-        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={onSuggestionsClearRequested}
-        getSuggestionValue={getSuggestionValue}
-        renderSuggestion={renderSuggestion}
-        inputProps={inputProps}
+    <Autocomplete
+      value={value}
+    
+      onChange={(event, newValue) => {
+        if (typeof newValue === 'string') {
+          setInputValue(newValue)
+        } else if (newValue && newValue.inputValue) {
+          // Create a new value from the user input
+          setInputValue(newValue.inputValue)
+        } else {
+          setValue(newValue);
+        }
+      }}
+      filterOptions={(options, params) => {
+        const filtered = filter(options, params);
+
+        // Suggest the creation of a new value
+        if (params.inputValue !== '') {
+          filtered.push({
+            inputValue: params.inputValue,
+            title: `Add "${params.inputValue}"`,
+          });
+        }
+
+        return filtered;
+      }}
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+      id="free-solo-with-text-demo"
+      options={top100Films} 
+      getOptionLabel={(option) => {
+        // Value selected with enter, right from the input
+        if (typeof option === 'string') {
+          return option;
+        }
+        // Add "xxx" option created dynamically
+        if (option.inputValue) {
+          return option.inputValue;
+        }
+        // Regular option
+        return option.title;
+      }}
+      renderOption={(option) => option.title}
+      style={{ width: 400, backgroundColor: "white" }}
+      freeSolo
+      renderInput={(params) => (
+        <TextField {...params} label="Free solo with text demo" variant="outlined" />
+      )}
     />
-    <input type="submit" value="&#128269;" data-testid="busqueda-recomendaciones-boton"/>
+     
+    <input type="submit" value="&#128269;" data-testid="busqueda-recomendaciones-boton"  style={{ height: 60, width: 60  }}/>
+    
     </Form>
     </Nav>
   
